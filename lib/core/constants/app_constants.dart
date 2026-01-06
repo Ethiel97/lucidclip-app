5f568ba8874f46e36b070d5a6fdfb22750f1c299
ethiel97 <ethiel97@gmail.com>
2026-01-06 17:13:37 -0500

    feat(env): add Supabase project ID and token to environment variables

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -8,12 +8,23 @@ class AppConstants {
   static final String supabaseProjectUrl =
       dotenv.env['SUPABASE_PROJECT_URL'] ??
       const String.fromEnvironment('SUPABASE_PROJECT_URL');
+
   static final supabaseAnonKey =
       dotenv.env['SUPABASE_ANON_KEY'] ??
       const String.fromEnvironment('SUPABASE_ANON_KEY');
+
+  static final supabaseProjectId =
+      dotenv.env['SUPABASE_PROJECT_ID'] ??
+      const String.fromEnvironment('SUPABASE_PROJECT_ID');
+
+  static final supabaseToken =
+      dotenv.env['SUPABASE_TOKEN'] ??
+      const String.fromEnvironment('SUPABASE_TOKEN');
+
   static final supabaseApiKey =
       dotenv.env['SUPABASE_API_KEY'] ??
       const String.fromEnvironment('SUPABASE_API_KEY');
+
   static final firebaseApiKey =
       dotenv.env['FIREBASE_API_KEY'] ??
       const String.fromEnvironment('FIREBASE_API_KEY');
